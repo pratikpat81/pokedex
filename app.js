2e1418bf76c720c1c8d2a04ae7e52945baf965ba
pratikpat81 <106554819+pratikpat81@users.noreply.github.com>
2022-07-20 21:23:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function start(){
- //NaaME
+ //NAME
     const pokemonName = "Lugia"
     const pokemonNameMessage = "Pokemon Name: " + pokemonName;
     document.getElementById("name").innerHTML = pokemonNameMessage
@@ -8,5 +8,22 @@ function start(){
 const imageURL = 
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png"
     document.getElementById("image").src = imageURL;
-
 }
+
+//MOVE LIST 
+const move1 = "headbutt"
+const move2 = "fly"
+const move3 = "gust"
+const move4 = "whirldwind"
+document.getElementById("move-list").innerHTML =
+`<li>${move1}</li> <li>${move2}</li> <li>${move3}</li> <li>${move1}</li>`
+
+const requestURL = "https://pokeapi.co/api/v2/pokemon/lugia";
+//JS Promises 
+
+fetch (requestURL)
+.then((x) => x.json())
+.then((obj)) => { 
+    console.log(obj);
+
+}); 
