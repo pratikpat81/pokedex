964cae850a68d450248f001063a6ff483de40f71
pratikpat81 <106554819+pratikpat81@users.noreply.github.com>
2022-07-20 22:34:25 -0400

    changed to use API fetch data

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
 function start(){
- //NAME
-    const pokemonName = "Lugia"
-    const pokemonNameMessage = "Pokemon Name: " + pokemonName;
-    document.getElementById("name").innerHTML = pokemonNameMessage
-
-//IMAGE
-const imageURL = 
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/249.png"
-    document.getElementById("image").src = imageURL;
-}
 
 //MOVE LIST 
-const move1 = "headbutt"
-const move2 = "fly"
-const move3 = "gust"
-const move4 = "whirldwind"
-document.getElementById("move-list").innerHTML =
-`<li>${move1}</li> <li>${move2}</li> <li>${move3}</li> <li>${move1}</li>`
+let move1 = "Headbutt"
+let move2 = "Fly"
+let move3 = "Gust"
+let move4 = "Whirlwind"
+document.getElementById(
+    "move-list"
+    ).innerHTML =`<li>${move1}</li><li>${move2}</li><li>${move3}</li><li>${move4}</li>`
 
-const requestURL = "https://pokeapi.co/api/v2/pokemon/lugia";
-//JS Promises 
-
-fetch (requestURL)
+const requestURL = "https://pokeapi.co/api/v2/pokemon/charmander"
+//JAVASCRIPT PROMISES 
+fetch(requestURL)
 .then((x) => x.json())
-.then((obj)) => { 
-    console.log(obj);
+.then((obj) => { 
+const name = obj.name;
+const pokemonNameMessage = "Pokemon Name: " + name;
+document.getElementById("name").innerHTML = pokemonNameMessage
 
-}); 
+//IMAGE
+const imageURL = obj.sprites.front_default;
+document.getElementById("image").src = imageURL;
+
+//MOVE LIST 
+let move1 = obj.moves[0].move.name;
+let move2 = obj.moves[1].move.name;
+let move3 = obj.moves[2].move.name;
+let move4 = obj.moves[4].move.name;
+document.getElementById(
+    "move-list"
+    ).innerHTML =`<li>${move1}</li><li>${move2}</li><li>${move3}</li><li>${move4}</li>`
+
+
+});
+
+} 
